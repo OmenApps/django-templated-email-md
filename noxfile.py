0e7d68b162dede02bce1451fd4a1299b37457ff4
jacklinke <jacklinke@gmail.com>
2025-10-22 23:59:13 -0400

    Update dependencies and minimum supported Python/Django versions

--- noxfile.py
+++ noxfile.py
@@ -12,17 +12,18 @@ from nox.sessions import Session
 
 # DJANGO_STABLE_VERSION should be set to the latest Django LTS version
 
-DJANGO_STABLE_VERSION = "5.1"
+DJANGO_STABLE_VERSION = "5.2"
 DJANGO_VERSIONS = [
     "4.2",
     "5.0",
     "5.1",
+    "5.2",
 ]
 
 # PYTHON_STABLE_VERSION should be set to the latest stable Python version
 
-PYTHON_STABLE_VERSION = "3.12"
-PYTHON_VERSIONS = ["3.10", "3.11", "3.12"]
+PYTHON_STABLE_VERSION = "3.14"
+PYTHON_VERSIONS = ["3.11", "3.12", "3.13", "3.14"]
 
 
 PACKAGE = "django_templated_email_md"
